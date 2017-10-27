4e5013150aba42d79fa4a8b308d0371c4933e192
Steve Wake <steve@moveforward.co.nz>
2017-10-28 00:10:23 +1300

    added autoloader to composer

--- loader.php
+++
@@ -1,8 +0,0 @@
-<?php
-// until can get into module and autoload properly
-// this should be required in your application _config.php
-require_once( str_replace( '/', DIRECTORY_SEPARATOR, __DIR__ . '/code/traits/config.php' ) );
-require_once( str_replace( '/', DIRECTORY_SEPARATOR, __DIR__ . '/code/traits/field.php' ) );
-require_once( str_replace( '/', DIRECTORY_SEPARATOR, __DIR__ . '/code/traits/requirements.php' ) );
-require_once( str_replace( '/', DIRECTORY_SEPARATOR, __DIR__ . '/code/traits/select2field.php' ) );
-
